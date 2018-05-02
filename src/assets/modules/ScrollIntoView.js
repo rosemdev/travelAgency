180329ce0596fd30bc9886295ad68d85ecf885b1
RomSem <romasemenyshyn@gmail.com>
2018-05-02 22:54:02 +0300

    changed scroll into view method

--- src/assets/modules/ScrollIntoView.js
+++ src/assets/modules/ScrollIntoView.js
@@ -5,18 +5,25 @@ export default function ScrollIntoView() {
     navigation.addEventListener("click", function (event) {
         let sections = document.querySelectorAll(".section"),
             menuItems = navigation.querySelectorAll("li"),
-            links = navigation.querySelectorAll("a"),
             clickedItem = event.target.closest("li");
 
         for (let i = 0; i < sections.length; i++) {
             let section = sections[i],
-                menuItem = menuItems[i],
-                link = links[i];
+                menuItem = menuItems[i];
+
+            let offset = 30,
+                coordToScroll = section.getBoundingClientRect().top - offset;
 
             if (clickedItem === menuItem) {
-                section.scrollIntoView({behavior: "smooth", block: "start", inline: "center"});
+                window.scrollBy({
+                    behavior: "smooth",
+                    top: coordToScroll,
+                    left: 0
+                });
             }
         }
     });
 
 }
+//alternative option
+// ({section.scrollIntoView({behavior: "smooth", block: "start", inline: "center"});
